fec567d8c5ca3c9a5267623e51f949d91cc3f150
alan <p90020002@yahoo.com.tw>
2016-10-20 23:28:28 +0800

    練習

---
+++ app/src/main/java/alan/no2/ys101902/MainActivity.java
@@ -0,0 +1,13 @@
+package alan.no2.ys101902;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
